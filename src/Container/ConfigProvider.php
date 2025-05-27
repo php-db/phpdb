9ebb415e94513664c2a0094f8cb7b23d602ac0dd
Joey Smith <jsmith@webinertia.net>
2025-05-27 00:58:24 -0500

    Derp, gotta have a factory mapping Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/Container/ConfigProvider.php
+++ src/Container/ConfigProvider.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Laminas\Db\Container;
 
+use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Db\Adapter\AdapterServiceFactory;
+
 final class ConfigProvider
 {
     public function __invoke(): array
@@ -17,6 +20,7 @@ final class ConfigProvider
     {
         return [
             'factories' => [
+                AdapterInterface::class => AdapterServiceFactory::class,
                 AdapterManager::class => AdapterManagerFactory::class,
             ],
         ];
