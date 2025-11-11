8c15bbb3b4559870b7a29afa243e4a4a08fefaba
Simon Mundy <simon.mundy@peptolab.com>
2025-11-11 17:02:05 +1100

    Fix merge issues: restore TYPE constants and remove gitignore copy
    - Added TYPE constants back to ExpressionInterface (required by existing code)
    - Removed '.gitignore copy' file (merge artifact)

--- src/Sql/ExpressionInterface.php
+++ src/Sql/ExpressionInterface.php
@@ -4,5 +4,10 @@ namespace PhpDb\Sql;
 
 interface ExpressionInterface
 {
+    public const TYPE_IDENTIFIER = 'identifier';
+    public const TYPE_VALUE      = 'value';
+    public const TYPE_LITERAL    = 'literal';
+    public const TYPE_SELECT     = 'select';
+
     public function getExpressionData(): ExpressionData;
 }
