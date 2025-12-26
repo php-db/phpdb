7ad36e1e4fbc97888e1a6cc681b10787ab8bdcc5
Joey Smith <jsmith@webinertia.net>
2025-12-26 03:13:32 -0600

    Adds final public const ConfigProvider::NAMED_ADAPTER_KEY for targeting the configuration key used by the AbstractAdapterInterfaceFactory Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -6,10 +6,26 @@ namespace PhpDb;
 
 final class ConfigProvider
 {
+    public final const NAMED_ADAPTER_KEY = 'adapters';
     public function __invoke(): array
     {
         return [
-            'dependencies' => $this->getDependencies(),
+            'dependencies'                  => $this->getDependencies(),
+            Adapter\AdapterInterface::class => $this->getConfig(),
+        ];
+    }
+
+    public function getConfig(): array
+    {
+        // supported configuration structure
+        return [
+            // Adapter\Adapter::class          => [],
+            // Adapter\AdapterInterface::class => [],
+            // self::NAMED_ADAPTER_KEY         => [
+            //     Adapter\Adapter::class          => [],
+            //     Adapter\AdapterInterface::class => [],
+            //     'Custom\Name'                   => [],
+            // ],
         ];
     }
 
---
+++ src/Exception/ContainerException.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpDb\Exception;
+
+use Psr\Container\ContainerExceptionInterface;
+use RuntimeException as SplRuntimeException;
+
+final class ContainerException extends SplRuntimeException implements ContainerExceptionInterface
+{
+    public static function forService(
+        string $serviceName,
+        string $factoryClass,
+        string $reason
+    ): self {
+        return new self(
+            sprintf(
+                'Failed to create service "%s" in factory %s Reason: %s',
+                 $serviceName,
+                 $factoryClass,
+                 $reason
+            ),
+            0,
+        );
+    }
+}
