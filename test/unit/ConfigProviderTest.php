0eceb268b1a0a75f049564fbb64d0525e93abde5
Joey Smith <jsmith@webinertia.net>
2026-01-08 22:11:06 -0600

    Delegates adapter creation back to php-db/phpdb Updates existing test Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- test/unit/ConfigProviderTest.php
+++ test/unit/ConfigProviderTest.php
@@ -6,20 +6,31 @@ namespace PhpDbTest;
 
 use PhpDb\Adapter;
 use PhpDb\ConfigProvider;
-use PhpDb\Container\AbstractAdapterInterfaceFactory;
+use PhpDb\Container;
 use PHPUnit\Framework\TestCase;
 
 class ConfigProviderTest extends TestCase
 {
-    /** @phpstan-var array{'dependencies': array{abstract_factories: list<class-string>, aliases: array<class-string, class-string>}} */
+    /**
+     * @phpstan-var array{
+     *      'dependencies': array{
+     *          abstract_factories: list<class-string>,
+     *          aliases: array<class-string, class-string>,
+     *          factories: array<class-string, class-string>,
+     *      }
+     * }
+     * */
     private array $config = [
         Adapter\AdapterInterface::class => [],
         'dependencies'                  => [
             'abstract_factories' => [
-                AbstractAdapterInterfaceFactory::class,
+                Container\AbstractAdapterInterfaceFactory::class,
             ],
             'aliases'            => [
                 Adapter\AdapterInterface::class => Adapter\Adapter::class,
+            ],
+            'factories'          => [
+                Adapter\Adapter::class => Container\AdapterInterfaceFactory::class,
             ],
         ],
     ];
